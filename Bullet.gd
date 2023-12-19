2f446f3a329e2aaefa1f1b4ffe0d19a822973512
Alexander Schmidt <aschmid3@my.westga.edu>
2023-12-19 15:34:01 -0500

    Added Health to Bat

--- Bullet.gd
+++ Bullet.gd
@@ -15,5 +15,6 @@ func _physics_process(delta):
 
 
 func _on_body_entered(body):
-	body.queue_free()
+	if body is Bat:
+		body.health -= 1
 	queue_free()
--- Enemy/Bat/Bat.gd
+++ Enemy/Bat/Bat.gd
@@ -3,6 +3,19 @@ extends CharacterBody2D
 
 var target: Player = null
 
+var _health: int = 5
+
+signal died(bat: Bat)
+
+var health: int:
+	get:
+		return _health
+	set(value):
+		_health = value
+		if _health <= 0:
+			died.emit(self)
+
+
 func _physics_process(delta):
 	if target != null:
 		velocity = (target.position - position).normalized() * 100
