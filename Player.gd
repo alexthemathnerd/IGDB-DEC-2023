dd7d1495047950b5bf04226eedea060b8dd6c559
daniel4255 <mysticglacial@icloud.com>
2023-12-20 05:39:14 -0500

    Added Player hit state and player health and updated state machine functions

--- Player.gd
+++ Player.gd
@@ -1,16 +1,19 @@
 class_name Player
 extends CharacterBody2D
 
-enum States {IDLE, WALKING, DEATH}
+enum States {IDLE, WALKING, DEATH, HIT}
 
 @onready var animation_player = $CollisionShape2D/Graphics/Animations
 
+@export var health: int = 50
 @export var bullet_scene: PackedScene
 @export var move_speed = 200
 @export var shoot_distance = 10000
 
 var _state : int = States.IDLE
+var _move_dir: Vector2 = Vector2.ZERO
 
+var in_hit_state = false
 var dead = false
 
 func _ready():
@@ -25,20 +28,23 @@ func _process(_delta):
 		
 
 func _physics_process(_delta):
-	if dead:
+	if dead or in_hit_state:
 		return
-	var move_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	_move_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	if move_dir.x != 0:
-		$CollisionShape2D/Graphics/Animations.flip_h = move_dir.x < 0
-		
-	if move_dir != Vector2.ZERO:
-		set_state(States.WALKING)
-		velocity = move_dir * move_speed
-		move_and_slide()
+	if _move_dir.x != 0:
+		$CollisionShape2D/Graphics/Animations.flip_h = _move_dir.x < 0
+	update_state_based_on_movement()
+	velocity = _move_dir * move_speed
+	move_and_slide()
+
+
+func take_damage(damage_amount: int):
+	health -= damage_amount
+	if health <= 0:
+		die()
 	else:
-		set_state(States.IDLE)
-			
+		set_state(States.HIT)
 
 func die():
 	if dead:
@@ -54,6 +60,11 @@ func shoot():
 	bullet_instance.direction = direction
 	get_parent().add_child(bullet_instance)
 
+func update_state_based_on_movement():
+	if _move_dir != Vector2.ZERO:
+		set_state(States.WALKING)
+	else:
+		set_state(States.IDLE)
 
 
 func set_state(new_state: int):
@@ -65,8 +76,16 @@ func set_state(new_state: int):
 			animation_player.play("Walk")
 		States.DEATH:
 			animation_player.play("Death")
-			die()
+		States.HIT:
+			animation_player.play("Hit")
+			in_hit_state = true
 
 
 func _on_death_timer_timeout():
 	set_state(States.DEATH)
+
+
+func _on_animations_animation_finished(anim_name: String):
+	if anim_name == "Hit":
+		in_hit_state = false
+		update_state_based_on_movement()
