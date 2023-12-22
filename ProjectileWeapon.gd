a4ffe5ddab51c1dc90a8df15866d9b759f422e3c
daniel4255 <mysticglacial@icloud.com>
2023-12-22 03:38:18 -0500

    Added automatic weapon and fixed bullet despawn timer.

--- ProjectileWeapon.gd
+++ ProjectileWeapon.gd
@@ -1,14 +1,27 @@
+class_name projectWeapon
 extends Node2D
 
 @export var bullet_scene: PackedScene
-
+@onready
+var fire_rate_timer: Timer = $FireRateTimer
+var is_firing = false
+var first_shot_fired = false
 @onready
 var animated_sprite = $AnimatedSprite2D
+
+func _ready():
+	set_fire_rate(0.15)
 
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
-	
+		first_shot_fired = false
+		fire_rate_timer.start()
+	if Input.is_action_just_pressed("shoot") and not first_shot_fired:
+		is_firing = true
+	if Input.is_action_just_released("shoot"):
+		is_firing = false
+		fire_rate_timer.stop()
 
 	var mouse_pos = get_global_mouse_position()
 	var weapon_pos = global_position
@@ -30,3 +43,11 @@ func shoot():
 	bullet_instance.global_position = position
 	bullet_instance.direction = direction
 	get_parent().add_child(bullet_instance)
+
+func set_fire_rate(new_rate: float):
+	fire_rate_timer.wait_time = new_rate
+
+func _on_fire_rate_timer_timeout():
+	if is_firing:
+		first_shot_fired = true
+		shoot()
